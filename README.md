f98690a86a439dfaa66ca28e289b3a7641eb8ba9
Meredith Von Feldt <meredithvf@gmail.com>
2021-01-24 18:59:20 -0700

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-<h1 id="Copyright & license">License:</h1>
-
-<p>This was adapted from open source project by Abdul Qadir</p>
--- src/ApolloProvider.js
+++
@@ -1,9 +0,0 @@
-import React, { Suspense } from "react";
-import Loader from "./Components/common/Loader.js";
-const App = React.lazy(() => import("./App"));
-
-export default (
-  <Suspense fallback={<Loader />}>
-    <App />
-  </Suspense>
-);
